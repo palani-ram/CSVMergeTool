25a966d7aacdfd96e3dfba5b0ed49b4bae3f6ec1
Palanisamy Ramalingam <palani-ram@users.noreply.github.com>
2017-12-18 07:14:11 -0600

    Added CSV merge tool codes

---
+++ CSVMergeTool/CSVMergeTool/CSVHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace CSVMergeTool
+{
+
+	public static class CSVHelpers
+	{
+		public static void CombineCsvFiles(string sourceFolder, string destinationFile, string searchPattern = "*.csv")
+		{
+				// Specify wildcard search to match CSV files that will be combined
+				string[] filePaths = Directory.GetFiles(sourceFolder, searchPattern);
+
+				CombineCsvFiles(filePaths, destinationFile);
+			
+		}
+
+		public static void CombineCsvFiles(string[] filePaths, string destinationFile)
+		{
+			StreamWriter fileDest = new StreamWriter(destinationFile, true);
+
+			int i;
+			for (i = 0; i < filePaths.Length; i++)
+			{
+				string file = filePaths[i];
+
+				string[] lines = File.ReadAllLines(file);
+
+				if (i > 0)
+				{
+					lines = lines.Skip(1).ToArray(); // Skip header row for all but first file
+				}
+
+				foreach (string line in lines)
+				{
+					fileDest.WriteLine(line);
+				}
+			}
+
+			fileDest.Close();
+		}
+	}
+
+	public static class DataTableHelper
+	{
+		public static DataTable ToDataTable(this Dictionary<string, List<object>> dict)
+		{
+			DataTable dataTable = new DataTable();
+
+			dataTable.Columns.AddRange(dict.Keys.Select(c => new DataColumn(c)).ToArray());
+
+			for (int i = 0; i < dict.Values.Max(item => item.Count()); i++)
+			{
+				DataRow dataRow = dataTable.NewRow();
+
+				foreach (var key in dict.Keys)
+				{
+					if (dict[key].Count > i)
+						dataRow[key] = dict[key][i];
+				}
+				dataTable.Rows.Add(dataRow);
+			}
+
+			return dataTable;
+		}
+
+		public static void ToCSV(this DataTable dt, string destinationfile)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+											  Select(column => column.ColumnName);
+			sb.AppendLine(string.Join(",", columnNames));
+
+			foreach (DataRow row in dt.Rows)
+			{
+				IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+				sb.AppendLine(string.Join(",", fields));
+			}
+
+			File.WriteAllText(destinationfile, sb.ToString());
+		}
+	}
+
+}
---
+++ CSVMergeTool/CSVMergeTool/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVMergeTool
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			folderDlg.ShowNewFolderButton = true;
+			// Show the FolderBrowserDialog.
+			DialogResult result = folderDlg.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				textBox1.Text = folderDlg.SelectedPath;
+				Environment.SpecialFolder root = folderDlg.RootFolder;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var srcPath = textBox1.Text;
+			var destinationFile = textBox1.Text + "/CombinedCSV" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
+
+			if (string.IsNullOrEmpty(srcPath)){
+				MessageBox.Show("Select Folder path for CSV files", "Alert");
+				return;
+			}
+
+			try
+			{
+				CSVHelpers.CombineCsvFiles(srcPath, destinationFile);
+				MessageBox.Show("CSV Files merged successfully", "Success");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error");
+				return;
+			}
+		}
+	}
+}
